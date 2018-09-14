eac063eecdec5382f9ccb714a934f07eb20b93d1
borgified <borgified@gmail.com>
2018-09-14 10:57:32 -0700

    test travis integration w/ bs

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ desired_cap = {
  'os_version': '7.0'
 }
 
-username=os.environ.get('BROWSERSTACK_USERNAME')
-access_key=os.environ.get('BROWSERSTACK_ACCESS_KEY')
+username=os.environ.get('BROWSERSTACK_USER')
+access_key=os.environ.get('BROWSERSTACK_KEY')
 
 driver = webdriver.Remote(
     command_executor='http://{}:{}@hub.browserstack.com:80/wd/hub'.format(username,access_key),
